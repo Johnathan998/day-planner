dbb73f624fa7cb0bd27b69283b4dcc5cfde023fd
johnathan olivas <jj.galvan13@yahoo.com>
2019-12-19 15:51:09 -0700

    started script.js code over and added jquery function

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
-var currentHour = moment().hours()
 $(document).ready(function () {
+    var description = $('.description');
+    $('.saveBtn').on('click', function () {
+        makeKeys()
 
-    $(".saveBtn").on("click", function () {
-        savedEvent = $(this).prev().val();
-        localStorage.setItem($(".description").val(), savedEvent);
     });
-    var currentHour = moment().hours()
-    function hoursUpdater() {
-
-        console.log(currentHour);
-    }
-
-    $(".time-block").each(function () {
-        var blockHour = $(this).attr("id").split("-")[1]
-        if (blockHour < currentHour) {
-            console.log("Block Hour is before current hour");
-            $(".time-block").css("background-color", "grey");
-        } else if (blockHour > currentHour) {
-            $(".time-block").css("background-color", "red");
-        } else {
-            $(".time-block").css("background-color", "green");
-        }
-    });
-
-    $(".description").val(localStorage.getItem($(this).val()));
-
-});
