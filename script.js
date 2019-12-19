02c9ab30a090b9d99f232e2e2c68155fb113ef1b
johnathan olivas <jj.galvan13@yahoo.com>
2019-12-19 16:35:47 -0700

    added clearkeys function and hoursUpdater function

--- script.js
+++ script.js
@@ -42,3 +42,38 @@ $(document).ready(function () {
             localStorage.setItem('event-9', comment8)
         }
     }
+    function print() {
+        $('#event-9').val(localStorage.getItem('event-1'))
+        $('#event-10').val(localStorage.getItem('event-2'))
+        $('#event11').val(localStorage.getItem('event-3'))
+        $('#event-12').val(localStorage.getItem('event-4'))
+        $('#event-13').val(localStorage.getItem('event-5'))
+        $('#event-14').val(localStorage.getItem('event-6'))
+        $('#event-15').val(localStorage.getItem('event-7'))
+        $('#event-16').val(localStorage.getItem('event-8'))
+        $('#event-17').val(localStorage.getItem('event-9'))
+    }
+    function clearKeys() {
+        localStorage.clear();
+    }
+    function hoursUpdater() {
+        print();
+        var currentHour = moment().hours();
+        $('.time-block').each(function () {
+            var blockHour = parseInt($(this).attr('id').split('-')[1]);
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+            } else if (blockHour > currentHour) {
+                $(this).addClass("future");
+            } else {
+                $(this).addClass("present");
+            }
+        })
+        if (currentHour <= 8 || currentHour >= 18) {
+            clearKeys()
+        }
+    }
+
+    hoursUpdater();
+    var interval = setInterval(hoursUpdater, 15000);
+});
