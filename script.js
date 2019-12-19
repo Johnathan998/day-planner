fb5b794358b8589eeba6db280792378b5e9227ad
johnathan olivas <jj.galvan13@yahoo.com>
2019-12-18 19:50:39 -0700

    added time block and else if and else

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
+var currentHour = moment().hours()
 $(document).ready(function () {
+
     $(".saveBtn").on("click", function () {
-        console.log($(this))
-    })
+        savedEvent = $(this).prev().val();
+        localStorage.setItem($(".description").val(), savedEvent);
+    });
+    var currentHour = moment().hours()
     function hoursUpdater() {
-        var currentHour = moment().hours();
+
         console.log(currentHour);
-        $(".time-block").each(function () {
-            var blockHour = $(this).attr("id").split("-")[1]
-            if (blockHour < currentHour) {
-                console.log("Block Hour is before current hour");
-                $(".time-block").css("background-color", "grey")
-            } else if {
-
-            } else {
-
-            }
-        })
     }
-    hoursUpdater();
-});
 
+    $(".time-block").each(function () {
+        var blockHour = $(this).attr("id").split("-")[1]
+        if (blockHour < currentHour) {
+            console.log("Block Hour is before current hour");
+            $(".time-block").css("background-color", "grey");
+        } else if (blockHour > currentHour) {
+            $(".time-block").css("background-color", "red");
+        } else {
+            $(".time-block").css("background-color", "green");
+        }
+    });
+
+    $(".description").val(localStorage.getItem($(this).val()));
+
+});
